a5a145fcd97ab3a191ce76bce14bc06e64400a3d
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-09-24 19:11:05 -0700

    Add match class check

--- final_project.py
+++ final_project.py
@@ -1,2 +1,6 @@
 class Team():
     pass
+
+
+class Match(Team):
+    pass
--- test_final_project.py
+++ test_final_project.py
@@ -4,7 +4,13 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_team_class(self):
         import final_project
+        self.assertTrue(True, "loaded the module")
         team = final_project.Team()
+
+    def test_match_class(self):
+        import final_project
+        match = final_project.Match()
+        self.assertIsInstance(match, final_project.Match)
 
 
 if __name__ == "__main__":

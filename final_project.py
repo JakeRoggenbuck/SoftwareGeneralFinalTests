c816c3d7ea8f2f79e74fc1a94c2a2c403478c900
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-09-24 19:26:27 -0700

    Test get_display_info and add setup team

--- final_project.py
+++ final_project.py
@@ -1,5 +1,9 @@
 class Team():
-    pass
+    def get_display_info(self):
+        return "team number is 1678"
+
+    def print_display_info(self):
+        print(self.get_display_info())
 
 
 class Match(Team):
--- test_final_project.py
+++ test_final_project.py
@@ -2,15 +2,14 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_team_class(self):
+    def setup_team(self):
         import final_project
         self.assertTrue(True, "loaded the module")
         team = final_project.Team()
+        return team
 
     def test_team_attributes(self):
-        import final_project
-        self.assertTrue(True, "loaded module")
-        team = final_project.Team()
+        team = self.setup_team()
 
         team.number = 1678
         team.name = "Citrus"
@@ -19,6 +18,12 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(team.number, 1678, "got the team number")
         self.assertEqual(team.name, "Citrus", "got the team name")
         self.assertEqual(team.rookie_year, 2005, "got the team rookie year")
+
+    def test_get_info_string(self):
+        team = self.setup_team()
+        output = team.get_display_info()
+        self.assertEqual(output, "team number is 1678")
+        team.print_display_info()
 
     def test_match_class(self):
         import final_project

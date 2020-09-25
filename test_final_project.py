eccde5775d93b10a440ecb966ba3cd9bcfcc75c2
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-09-24 19:08:45 -0700

    Add test for checking team class

--- test_final_project.py
+++ test_final_project.py
@@ -2,8 +2,9 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertAlmostEqual(True, True)
+    def test_team_class(self):
+        import final_project
+        team = final_project.Team()
 
 
 if __name__ == "__main__":
